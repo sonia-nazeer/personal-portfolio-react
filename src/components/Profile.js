7091b9ccdbd245889446fdc3fc981552b26ceb57
sonia-nazeer <nazeersonia62@gmail.com>
2025-08-05 11:22:21 +0500

    updating css

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,34 +1,37 @@
+// src/components/Profile.js
 import React from 'react';
 import ProfileImage from '../assets/home-hero.jpg';
 import './profile.css';
 
 function Profile() {
   return (
-    <>
-      <section>
-        <div id="profile" className="container py-5">
-          <div className="row align-items-center">
-            <div className="col-md-6 left">
-              <h2>Hi, I am</h2>
-              <h1>Sonia Nazeer</h1>
-              <p>
-                Full Stack Web Developer | MERN & PHP/MySQL I build scalable,
-                responsive websites with clean code and modern design — focused
-                on performance, usability, and real-world functionality.
-              </p>
-              <a href='media/my-cv.pdf' download="My_CV" className="button">
-                Download My CV
-              </a>
-            </div>
+    <section>
+      <div id="profile" className="container py-5">
+        <div className="row align-items-center">
+          <div className="col-md-6 left">
+            <h2>Hi, I am</h2>
+            <h1>Sonia Nazeer</h1>
+            <p>
+              Full Stack Web Developer | MERN & PHP/MySQL I build scalable,
+              responsive websites with clean code and modern design — focused
+              on performance, usability, and real-world functionality.
+            </p>
+            <a
+              href={`${process.env.PUBLIC_URL}/media/my-cv.pdf`}
+              download="My_CV"
+              className="button"
+            >
+              Download My CV
+            </a>
+          </div>
 
-            {/* Right Column */}
-            <div className="col-md-6 right mt-4 mt-md-0 text-center profile-img">
-              <img src={ProfileImage} alt="Sonia Nazeer" />
-            </div>
+          {/* Right Column */}
+          <div className="col-md-6 right mt-4 mt-md-0 text-center profile-img">
+            <img src={ProfileImage} alt="Sonia Nazeer" />
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
 
